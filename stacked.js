e5fa07b174a73be2145ccd8bcc90bc84dca28041
Amil Kumarasinghe <akumarasinghe@geomatic.com.au>
2017-09-11 13:03:39 +1000

    Make Two Flows

--- stacked.js
+++ stacked.js
@@ -2,55 +2,76 @@ var svg = d3.select("body").append("svg")
     .attr("width", 500)
     .attr("height", 500);
 
-var group = svg.append("g")
-    .attr("transform", "translate(100,100)");
-
-var path = group.append("path")
-    .attr("d", "M 0 0, V 100, C 0 150, 0 150, 50 150, H 400")
-    .attr("fill", "none")
-    .attr("stroke", "red")
-    .attr("stroke-width", "4")
-    .attr("stroke-linecap", "round");
-
-var length = path.node().getTotalLength();
-console.log('length', length);
-var gap = 20;
-console.log('gap', gap);
-var noOfArrows = parseInt(length / gap);
-console.log('number of arrows', noOfArrows);
-
 var timeTakenPerPixel = 20;
-var duration = length * timeTakenPerPixel;
+var gap = 20;
 
-var arrow = group.selectAll("circle")
-    .data(d3.range(0, noOfArrows))
-    .enter()
-    .append("circle")
-    .attr("r", 5)
-    .attr("fill", "white")
-    .attr("stroke", "red")
-    .attr("stroke-width", "2")
-    .attr("stroke-linecap", "round");
+function renderOneFlow(group, path) {
+    
+    function transition() {
+        var p = circleSel.transition()
+            .duration(duration)
+            .ease(d3.easeLinear)
+            .attrTween("transform", translateAlongPath(path.node()))
+            .on("end", function () { d3.select(this).call(transition, false); });
+    }
 
-transition();
+    function translateAlongPath(path) {
+        return function (d, i, a) {
 
-function transition() {
-    var p = arrow.transition()
-        .duration(duration)
-        .ease(d3.easeLinear)
-        .attrTween("transform", translateAlongPath(path.node()))
-        .on("end", function () { d3.select(this).call(transition, false); });
-}
+            return function (timeFraction) {
+                var tOfThisArrow = timeFraction + (1 / noOfArrows) * i;
+                var l = (tOfThisArrow * length) % length;
+                var point = path.getPointAtLength(l);
+                return "translate(" + point.x + ", " + point.y + ")";
+            };
 
-function translateAlongPath(path) {
-    return function (d, i, a) {
-
-        return function (timeFraction) {
-            var tOfThisArrow = timeFraction + (1/noOfArrows) * i;
-            var l = (tOfThisArrow * length) % length;
-            var point = path.getPointAtLength(l);
-            return "translate(" + point.x + ", " + point.y + ")";
         };
+    }
 
-    };
+    var length = path.node().getTotalLength();
+    var noOfArrows = parseInt(length / gap);
+    var duration = length * timeTakenPerPixel;
+
+    var circleSel = group.selectAll("circle")
+        .data(d3.range(0, noOfArrows))
+        .enter()
+        .append("circle")
+        .attr("r", 5)
+        .attr("fill", "white")
+        .attr("stroke", "red")
+        .attr("stroke-width", "2")
+        .attr("stroke-linecap", "round");
+
+    transition();
 }
+
+
+function render() {
+    
+    var flow2Group = svg.append("g")
+        .attr("transform", "translate(100,100)");
+
+    var flow2Path = flow2Group.append("path")
+        .attr("d", "M 0 0, V 100, C 0 150, 0 150, 50 150, H 400")
+        .attr("fill", "none")
+        .attr("stroke", "red")
+        .attr("stroke-width", "4")
+        .attr("stroke-linecap", "round");
+    
+    renderOneFlow(flow2Group, flow2Path);
+
+
+    var flow2Group = svg.append("g")
+    .attr("transform", "translate(200,150)");
+
+    var flow2Path = flow2Group.append("path")
+        .attr("d", "M 0 0, V 100, C 0 150, 0 150, 50 150, V 400")
+        .attr("fill", "none")
+        .attr("stroke", "red")
+        .attr("stroke-width", "4")
+        .attr("stroke-linecap", "round");
+
+    renderOneFlow(flow2Group, flow2Path);
+}
+
+render();
